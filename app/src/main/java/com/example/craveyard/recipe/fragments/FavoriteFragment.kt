4aae00379ec1f84745fb93483900c2fc8a2eaeb9
Nada Mohammed <naddaa26@gmail.com>
2024-07-29 00:36:13 +0300

    added bottom navigation bar

--- app/src/main/java/com/example/craveyard/recipe/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/example/craveyard/recipe/fragments/FavoriteFragment.kt
@@ -7,17 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.craveyard.R
 
-
-class HomeFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
-
-
 
 }
--- app/src/main/java/com/example/craveyard/recipe/view/RecipeActivity.kt
+++ app/src/main/java/com/example/craveyard/recipe/view/RecipeActivity.kt
@@ -9,23 +9,33 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.craveyard.R
+import com.example.craveyard.recipe.fragments.FavoriteFragment
+import com.example.craveyard.recipe.fragments.HomeFragment
+import com.example.craveyard.recipe.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
-class RecipeActivity : AppCompatActivity() {
+class RecipeActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,0)
             insets
         }
+         // init toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.title = "CraveYard"
+
+        // init bottom nav bar
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
+        bottomNav.setOnItemSelectedListener(this)
+        bottomNav.setSelectedItemId(R.id.nav_home);
     }
-
-
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -39,9 +49,30 @@ class RecipeActivity : AppCompatActivity() {
                 return true
             }
             R.id.action_about -> {
-
+                return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragmentManager = supportFragmentManager
+        when (item.itemId) {
+            R.id.nav_home -> {
+                fragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+                return true
+            }
+            R.id.nav_search -> {
+                fragmentManager.beginTransaction().replace(R.id.fragment_container, SearchFragment()).commit()
+                return true
+            }
+            R.id.nav_favorites -> {
+                fragmentManager.beginTransaction().replace(R.id.fragment_container, FavoriteFragment()).commit()
+                return true
+            }
+        }
+        return false
+    }
+
+
 }
