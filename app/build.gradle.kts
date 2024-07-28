58449dc9726b2b72525ea071384999377aa7dfff
Nada Mohammed <naddaa26@gmail.com>
2024-07-28 15:34:21 +0300

    fix gradle module error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias { libs.plugins.safeargs }
+    alias (libs.plugins.safeargs)
     alias(libs.plugins.ksp)
 }
 
