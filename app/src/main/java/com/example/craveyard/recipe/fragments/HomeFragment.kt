a7fe466ffc1636701222982dcb50311460b4b16f
Nada Mohammed <naddaa26@gmail.com>
2024-07-29 09:25:42 +0300

    added home and favorite views

--- app/src/main/java/com/example/craveyard/recipe/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/craveyard/recipe/fragments/HomeFragment.kt
@@ -5,10 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.craveyard.R
+import com.example.craveyard.recipe.viewmodel.HomeViewModel
 
 
 class HomeFragment : Fragment() {
+    lateinit var viewModel: HomeViewModel
+
+    private fun initializeViewModel() {
+        viewModel = HomeViewModel()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initializeViewModel()
+
+//        viewModel.getMyResponse()
+//        viewModel.myResponse.observe(this) {
+//            val mealList = it.meal
+//            val recipesAdapter = MealAdapter(mealList)
+//            val rv = findViewById<RecyclerView>(R.id.rv)
+//            rv.adapter = recipesAdapter
+//            rv.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
+//        }
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
