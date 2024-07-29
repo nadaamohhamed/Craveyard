492496d2a1240fd16148bf05d55d57dbd9b761d2
Eslam <eslamm378@gmail.com>
2024-07-29 15:30:02 +0300

    OnMealItemClick Logic with Safe Args

---
+++ app/src/main/java/com/example/craveyard/recipe/clickhandler/ClickHandlerImplementation.kt
@@ -0,0 +1,12 @@
+package com.example.craveyard.recipe.clickhandler
+
+import com.example.craveyard.recipe.model.Meal
+import com.example.craveyard.recipe.search.view.SearchFragment
+
+class ClickHandlerImplementation : ClickHandler {
+    override fun onMealClick(meal: Meal) {
+//        SearchFragmentDirections.actionSearchFragmentToRecipeFragment(meal).also {
+//            SearchFragment.findNavController().navigate(it)
+//        }
+    }
+}
