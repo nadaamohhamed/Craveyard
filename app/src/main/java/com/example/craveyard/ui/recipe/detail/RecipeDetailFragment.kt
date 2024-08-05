0365ccbe973a1120db60149ed1401d1d43faadbb
ShinobiBoi <168246062+ShinobiBoi@users.noreply.github.com>
2024-08-05 19:37:07 +0300

    detail fav btn

--- app/src/main/java/com/example/craveyard/ui/recipe/detail/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/craveyard/ui/recipe/detail/RecipeDetailFragment.kt
@@ -1,19 +1,34 @@
 package com.example.craveyard.ui.recipe.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.craveyard.R
+import com.example.craveyard.data.db.localdata.LocalDs
+import com.example.craveyard.data.model.entity.FavMeal
+import com.example.craveyard.ui.recipe.favorite.repo.FavRepo
+import com.example.craveyard.ui.recipe.favorite.viewmodel.FavViewModel
+import com.example.craveyard.ui.recipe.favorite.viewmodel.FavViewModelFactory
+import com.example.craveyard.ui.recipe.utils.adapter.MealsAdapter.ViewHolder
 import com.example.craveyard.ui.recipe.utils.connection.ConnectionManager
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import kotlinx.coroutines.launch
 
 class RecipeDetailFragment : Fragment() {
 
@@ -25,6 +40,8 @@ class RecipeDetailFragment : Fragment() {
     lateinit var reciepeText:TextView
     lateinit var readmore:TextView
     lateinit var youTubePlayerView: YouTubePlayerView
+    lateinit var favBtn : ImageButton
+    lateinit var favViewModel:FavViewModel
 
 
     private val args : RecipeDetailFragmentArgs by navArgs()
@@ -45,8 +62,16 @@ class RecipeDetailFragment : Fragment() {
         reciepeText=view.findViewById(R.id.meal_ins)
         readmore=view.findViewById(R.id.readmore)
         youTubePlayerView=view.findViewById(R.id.youtube_view)
+        favBtn=view.findViewById(R.id.detail_image_Button)
 
         val meal=args.meal
+
+
+        val localDs= LocalDs(requireContext())
+        val favViewModelFactory = FavViewModelFactory(FavRepo(localDs))
+       favViewModel = ViewModelProvider(this, favViewModelFactory).get(FavViewModel::class.java)
+
+
 
         Glide.with(mealImage).load(meal.strMealThumb).into(mealImage)
         mealName.text=meal.strMeal
@@ -66,6 +91,40 @@ class RecipeDetailFragment : Fragment() {
 
 
         }
+
+
+        favViewModel.viewModelScope.launch {
+
+           val  isFavorite = favViewModel.isFavorite(meal, Firebase.auth.currentUser!!.email!!)
+
+                if (isFavorite) {
+
+                    favBtn.setImageResource(R.drawable.ic_favorite)
+                }
+                else {
+                    favBtn.setImageResource(R.drawable.ic_favorite_border)
+                }
+
+
+
+        }
+        favBtn.setOnClickListener(){
+            favViewModel.viewModelScope.launch {
+                val isFavorite = favViewModel.isFavorite(meal,Firebase.auth.currentUser!!.email!!)
+                if (isFavorite) {
+                    val favMeal=favViewModel.getMeal(Firebase.auth.currentUser!!.email!!,meal.idMeal)
+                    removeMealFromFavorites(favMeal)
+                } else {
+
+                    val favMeal= FavMeal(Firebase.auth.currentUser!!.email!!,meal.idMeal,meal.strMeal!!,
+                        meal.strArea,meal.strCategory,meal.strInstructions!!,meal.strYoutube, meal.strMealThumb!!)
+
+                    addMealToFavorites(favMeal)
+                }
+            }
+
+        }
+
 
         if(ConnectionManager.isNetworkAvailable(requireContext())){
             youTubePlayerView.visibility=View.VISIBLE
@@ -92,6 +151,30 @@ class RecipeDetailFragment : Fragment() {
         }
 
         return view
+    }
+
+
+    private fun addMealToFavorites(favMeal: FavMeal) {
+
+        favViewModel.insertFavMeal(favMeal)
+        favBtn.setImageResource(R.drawable.ic_favorite)
+
+        Toast.makeText(context, "Added to your favorites!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun removeMealFromFavorites(favMeal: FavMeal) {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Remove Favorite")
+            .setMessage("Are you sure you want to remove this meal from favorites?")
+            .setPositiveButton("Yes") { _, _ ->
+                favViewModel.deleteFavMeal(favMeal)
+                favViewModel.getFavMeals(Firebase.auth.currentUser!!.email!!)
+                favBtn.setImageResource(R.drawable.ic_favorite_border)
+
+                Toast.makeText(context, "Removed from your favorites!", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
 
